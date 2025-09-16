dfccd3346a7e60e77c06b92d17ede43cc439461d
hardikpy <hardik.patel8@appinventiv.com>
2025-09-16 14:59:44 +0530

    default terms

---
+++ bk_customization/models/purchase.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    use_purchase_order_note = fields.Boolean(
+        string='Default(s) Terms & Conditions')
+    purchase_order_note = fields.Html(
+        string='Default Term(s) and Condition(s)', translate=True)
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    use_purchase_order_note = fields.Boolean(
+        related='company_id.use_purchase_order_note', readonly=False,
+        string='Default(s) Terms & Conditions')
+    purchase_order_note = fields.Html(
+        related='company_id.purchase_order_note', readonly=False,
+        string="Conditions & Terms")
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    @api.model
+    def _default_note(self):
+        if self.env.user.company_id.use_purchase_order_note:
+            return self.env.user.company_id.purchase_order_note
+        return ''
+
+    note = fields.Html('Terms and Conditions', default=_default_note)
